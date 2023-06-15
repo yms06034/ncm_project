383b40773ee1c883249ed1434439fa20f35d1fe1
yms06034 <yms06034@gamil.com>
2023-06-15 16:45:15 +0900

    first add

---
+++ naverCafeMacro.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import pandas as pd
+import numpy as np
+import time
+
+def css_finds(css_selector):
+    return driver.find_elements(By.CSS_SELECTOR, css_selector)
+
+def css_find(css_selector):
+    return driver.find_element(By.CSS_SELECTOR, css_selector)
+
+def find(wait, css_selector):
+    return wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
+
+def finds_xpath(xpath):
+    return driver.find_elements(By.XPATH, xpath)
+
+def find_xpath(xpath):
+    return driver.find_element(By.XPATH, xpath)
+
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+options.add_argument('no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--start-maximized')
+options.add_argument('incognito')
+
+driver = webdriver.Chrome("./chromedriver.exe", options=options)
+driver.get("") # naverCafe URL
+wait = WebDriverWait(driver, 10)
